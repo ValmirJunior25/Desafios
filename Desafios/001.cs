a4e0e4fde15af7d82d8b3d7c3f1e8070bca786e1
ValmirJunior25 <valmirjunior1025@gmail.com>
2026-07-03 14:15:49 -0300

    Initial commit

---
+++ Desafios/001.cs
@@ -0,0 +1,16 @@
+using System;
+class desafio001
+{
+    static void Main()
+    {
+        string produto = "Caneta Azul";
+        decimal preco = 2.5m;
+        int estoque = 150;
+        bool disponivel = true;
+
+        Console.WriteLine($"Produto: {produto}");
+        Console.WriteLine($"Preço: {preco}");
+        Console.WriteLine($"Quantidade em estoque: {estoque}");
+        Console.WriteLine($"Disponível para venda: {disponivel}");
+    }
+}
