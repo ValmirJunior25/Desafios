ab04c132cd1455334759644e82a730f03bdf5eee
ValmirJunior25 <valmirjunior1025@gmail.com>
2026-07-05 23:29:40 -0300

    (apenas os mais interessantes) desafio 6

--- Desafios.cs
+++ Desafios.cs
@@ -1,9 +1,30 @@
 ﻿using System;
-class Desafios
+
+class Desafio
 {
     static void Main()
     {
-        Desafio005 d5 = new Desafio005();
-        d5.Desafio5();
+        Console.Clear();
+
+        int total = 0, positivos = 0;
+
+        Console.WriteLine("Digite 5 números inteiros.");
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.Write($"Digite o número {i}: ");
+            int num = int.Parse(Console.ReadLine() ?? "0");
+
+            total += num;
+
+            if (num > 0)
+            {
+                positivos++;
+            }
+        }
+
+        Console.WriteLine($"Soma total: {total}");
+        Console.WriteLine($"Média: {total / 5m}");
+        Console.WriteLine($"Números positivos: {positivos}");
+
     }
 }
