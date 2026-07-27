47b7d9edd101a5a61d1189850007ac37261e931a
ValmirJunior25 <valmirjunior1025@gmail.com>
2026-07-27 07:54:25 -0300

    Início desafio 02

--- Desafio02-Conversor/Program.cs
+++ Desafio02-Conversor/Program.cs
@@ -1,1 +1,50 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        bool on = true;
+        while (on)
+        {
+            Console.Clear();
+            Console.Write("=== Conversor de Medidas === \n1. Celsius → Fahrenheit \n2. Quilômetros → Milhas \n3. Quilogramas → Libras \n4. Sair");
+
+            string escolhaMenu = Console.ReadLine() ?? "";
+
+            switch (escolhaMenu)
+            {
+                case "1":
+
+                    break;
+
+
+                case "2":
+                
+                    break;
+
+
+                case "3":
+                
+                    break;
+
+
+                case "4":
+                
+                    Console.Write("Confirmar saida [s/n]: ");
+                    char saidaMenu = char.Parse((Console.ReadLine() ?? "n").ToLower().Trim());
+                    if (saidaMenu == 's')
+                    {
+                        on = false;
+                    }
+                    break;
+
+
+                default:
+                    break;
+            }
+
+
+        }
+    }
+}
