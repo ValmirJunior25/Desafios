445e322c68dcb966ae8890752b289c9cc56498c3
ValmirJunior25 <valmirjunior1025@gmail.com>
2026-07-30 11:41:40 -0300

    Desafio 07 - Tratamento de Exceções

---
+++ Desafio06-Heranca/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+class Prgram
+{
+    static void Main()
+    {
+        // Placa ABC-1234 - Modelo: Civic
+        //Placa XYZ-5678 - Modelo: Corolla
+        //Placa MOT-0001 - Modelo: CB500
+        //Placa MOT-0002 - Modelo: Biz
+
+
+        List<Veicolo> veicolos = [];
+
+        Carro v1 = new();
+        v1.Modelo = "Civic";
+        v1.Placa = "ABC-1234";
+        v1.NumPortas = 4;
+
+        Carro v2 = new() {Modelo = "Corolla", Placa = "XYZ-5678", NumPortas = 4};
+        Moto v3 = new() {Modelo = "CB500", Placa = "MOT-0001", Cilindrada = 500};
+        Moto v4 = new() {Modelo = "Biz", Placa = "MOT-0002", Cilindrada = 125};
+
+        veicolos.Add(v1);
+        veicolos.Add(v2);
+        veicolos.Add(v3);
+        veicolos.Add(v4);
+
+        foreach(Veicolo veicolo in veicolos)
+        {
+            veicolo.ExibirDados();
+        }
+    }
+
+}
+
+
+class Veicolo
+{
+    public string Placa = "";
+    public string Modelo = "";
+
+
+    public virtual void ExibirDados()
+    {
+        Console.WriteLine($"Placa: {Placa} - Modelo: {Modelo}");
+    }
+}
+
+
+class Carro : Veicolo
+{
+    public int NumPortas;
+
+    public override void ExibirDados()
+    {
+        Console.WriteLine($"Placa: {Placa} - Modelo: {Modelo} - Portas: {NumPortas}");
+    }
+}
+
+
+class Moto : Veicolo
+{
+    public int Cilindrada;
+
+    public override void ExibirDados()
+    {
+        Console.WriteLine($"Placa: {Placa} - Modelo: {Modelo} - Cilindrada: {Cilindrada}");
+    }
+}
---
+++ Desafio07-TratamentoExcecoes/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int numerador;
+        int denominador;
+        double res;
+
+        while (true)
+        {
+            try
+            {
+                Console.Write("Digite o Numerador: ");
+                numerador = int.Parse(Console.ReadLine());
+                Console.Write("Digite o Denominador: ");
+                denominador = int.Parse(Console.ReadLine());
+
+                res = (double)numerador/denominador;
+                Console.WriteLine($"{numerador} / {denominador} = {res}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine($"ERRO: numero não é válido.");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine($"ERRO: não é possível dividir por zero.");
+            }
+        }
+    }
+}
